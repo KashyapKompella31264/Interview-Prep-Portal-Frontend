0ceb8720f3edc907eba0d79226c313320fd87fec
kashyap <kashyapkompella80@gmail.com>
2025-07-20 22:13:04 +0530

    Initial commit

---
+++ interview_prep_portal/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return ( 
+        <div>
+            
+            <h1>Home page</h1>
+            <Navbar/>
+        </div>
+     );
+}
+ 
+export default Home;
